66b7f8d39037d8867f6f0751f7d87cd7d9643826
Greg <greg.colley@gmail.com>
2014-10-23 01:11:36 +0100

    updated footer with composer debug

--- application/views/_templates/footer.php
+++ application/views/_templates/footer.php
@@ -1,6 +1,7 @@
     <div class="footer">
         <!-- echo out the content of the SESSION via KINT, a Composer-loaded much better version of var_dump -->
         <!-- KINT can be used with the simple function d() -->
+        <?php //echo '<pre>';print_r($_SESSION);echo '</pre>'; ?>
         <?php d($_SESSION); ?>
     </div>
 </body>
